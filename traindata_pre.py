7354899f03649585da9c3e66a5e6eb898180ca19
Changlu Guo <40800824+clguo@users.noreply.github.com>
2025-07-07 17:51:33 +0200

    Update traindata_pre.py

--- traindata_pre.py
+++ traindata_pre.py
@@ -1,21 +1,15 @@
-
 import numpy as np
-
 import nibabel as nib
 import cv2
 import os
 import glob
-#
+from scipy.ndimage import zoom, gaussian_filter
+from scipy.interpolate import RegularGridInterpolator
+from scipy.fft import fftn, ifftn
+
 path1 = 'WMHdataset/training/Utrecht/'
 path2 = 'WMHdataset/training/Amsterdam/GE3T/'
 path3 = 'WMHdataset/training/Singapore/'
-
-import numpy as np
-from scipy.ndimage import zoom
-
-from scipy.ndimage import gaussian_filter
-from scipy.interpolate import RegularGridInterpolator
-
 
 def elasticDeform(imageVolume, maskVolume, sigma, alpha, interMethod='linear', extraMethod='nearest'):
     # Elastic deformation of 3D images using random displacement fields
@@ -28,75 +22,61 @@ def elasticDeform(imageVolume, maskVolume, sigma, alpha, interMethod='linear', e
     # extraMethod: extrapolation method such as 'linear', 'cubic', 'spline', 'nearest'
 
     # Generating a random displacement field
-    imageHeight, imageWidth, imageDepth = imageVolume.shape  # volume size
-    dx = 2 * np.random.rand(imageHeight, imageWidth, imageDepth) - 1  # ~U(-1, 1)
-    dy = 2 * np.random.rand(imageHeight, imageWidth, imageDepth) - 1  # ~U(-1, 1)
-    dz = 2 * np.random.rand(imageHeight, imageWidth, imageDepth) - 1  # ~U(-1, 1)
+    imageHeight, imageWidth, imageDepth = imageVolume.shape
+    dx = 2 * np.random.rand(imageHeight, imageWidth, imageDepth) - 1
+    dy = 2 * np.random.rand(imageHeight, imageWidth, imageDepth) - 1
+    dz = 2 * np.random.rand(imageHeight, imageWidth, imageDepth) - 1
 
     # Smoothing and scaling the field
-    kernelSize = 2 * np.ceil(2 * sigma) + 1  # square kernel size
-    dx = alpha * gaussian_filter(dx, sigma, mode='constant', cval=0)  # filtered and scaled
-    dy = alpha * gaussian_filter(dy, sigma, mode='constant', cval=0)  # filtered and scaled
-    dz = alpha * gaussian_filter(dz, sigma, mode='constant', cval=0)  # filtered and scaled
+    kernelSize = 2 * np.ceil(2 * sigma) + 1
+    dx = alpha * gaussian_filter(dx, sigma, mode='constant', cval=0)
+    dy = alpha * gaussian_filter(dy, sigma, mode='constant', cval=0)
+    dz = alpha * gaussian_filter(dz, sigma, mode='constant', cval=0)
 
-    # Applying the random displacement (elastic distortion) to the pixels
-    x, y, z = np.ogrid[:imageHeight, :imageWidth, :imageDepth]  # structured rectangular grid
+    # Applying the random displacement (elastic distortion)
+    x, y, z = np.ogrid[:imageHeight, :imageWidth, :imageDepth]
 
     # Elastic deformation for image volume
     F_image = RegularGridInterpolator((x[:, 0, 0], y[0, :, 0], z[0, 0, :]), imageVolume, method=interMethod, bounds_error=False, fill_value=None)
-    queryPoints_image = np.array([x + dx, y + dy, z + dz]).transpose((1, 2, 3, 0))  # query points for interpolation
-    imageVolume_deformed = F_image(queryPoints_image)  # interpolant function evaluation at query points
+    queryPoints_image = np.array([x + dx, y + dy, z + dz]).transpose((1, 2, 3, 0))
+    imageVolume_deformed = F_image(queryPoints_image)
 
     # Elastic deformation for mask volume
     F_mask = RegularGridInterpolator((x[:, 0, 0], y[0, :, 0], z[0, 0, :]), maskVolume, method='nearest', bounds_error=False, fill_value=None)
-    queryPoints_mask = np.array([x + dx, y + dy, z + dz]).transpose((1, 2, 3, 0))  # query points for interpolation
-    maskVolume_deformed = F_mask(queryPoints_mask)  # interpolant function evaluation at query points
+    queryPoints_mask = np.array([x + dx, y + dy, z + dz]).transpose((1, 2, 3, 0))
+    maskVolume_deformed = F_mask(queryPoints_mask)
 
     return imageVolume_deformed, maskVolume_deformed
-
-
-
-
-
-import numpy as np
-
-
-import numpy as np
-
 
 def biasField(imageVolume):
     # Random intensity inhomogeneity generation in 3D images
 
-    # imageVolume: 3D image volume
+    imageHeight, imageWidth, imageDepth = imageVolume.shape
+    X, Y, Z = np.meshgrid(np.arange(1, imageHeight + 1), np.arange(1, imageWidth + 1), np.arange(1, imageDepth + 1), indexing='ij')
+    x0 = np.random.randint(1, imageHeight + 1)
+    y0 = np.random.randint(1, imageWidth + 1)
+    z0 = np.random.randint(1, imageDepth + 1)
+    G = 1 - ((X - x0) ** 2 / (imageHeight ** 2) + (Y - y0) ** 2 / (imageWidth ** 2) + (Z - z0) ** 2 / (imageDepth ** 2))
 
-    # Generating a gradient field with intensity inhomogeneity
-    imageHeight, imageWidth, imageDepth = imageVolume.shape  # volume size
-    X, Y, Z = np.meshgrid(np.arange(1, imageHeight + 1), np.arange(1, imageWidth + 1), np.arange(1, imageDepth + 1), indexing='ij')  # structured rectangular grid
-    x0 = np.random.randint(1, imageHeight + 1)  # random x-center
-    y0 = np.random.randint(1, imageWidth + 1)  # random y-center
-    z0 = np.random.randint(1, imageDepth + 1)  # random z-center
-    G = 1 - ((X - x0) ** 2 / (imageHeight ** 2) + (Y - y0) ** 2 / (imageWidth ** 2) + (Z - z0) ** 2 / (imageDepth ** 2))  # elliptic gradient field
-
-    # Volume modification using the multiplicative inhomogeneity model
     imageVolume = G * imageVolume.astype(float)
 
     return imageVolume.astype(imageVolume.dtype)
 
 def resize_image_and_mask(image, mask, target_size):
-    # 获取原始图像和掩膜的尺寸
+    # Get the original size of the image and mask
     original_size = np.array(image.shape)
-    # 计算尺寸差异
+    # Calculate size difference
     size_diff = target_size - original_size
 
-    # 判断是否需要进行 padding 或裁剪操作
+    # Determine whether to perform padding or cropping
     if np.any(size_diff < 0):
-        # 裁剪图像和掩膜，移除超过目标尺寸的部分
+        # Crop image and mask by removing the excess parts
         start = -size_diff // 2
         end = start + target_size
         resized_image = image[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
         resized_mask = mask[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
     else:
-        # 创建新的图像和掩膜，进行 padding 操作
+        # Create new image and mask with padding
         pad_before = size_diff // 2
         pad_after = size_diff - pad_before
         pad_width = [(pad_before[i], pad_after[i]) for i in range(len(size_diff))]
@@ -105,38 +85,37 @@ def resize_image_and_mask(image, mask, target_size):
 
     return resized_image, resized_mask
 
-
 def restore_image_and_mask(resized_image, resized_mask, original_size):
-    # 计算尺寸差异
+    # Calculate size difference
     size_diff = original_size - np.array(resized_image.shape)
-    # 计算裁剪的边界
+    # Calculate crop boundaries
     start = -size_diff // 2
     end = start + original_size
-    # 还原图像和掩膜的尺寸
+    # Restore image and mask to original size
     restored_image = resized_image[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
     restored_mask = resized_mask[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
 
     return restored_image, restored_mask
-from scipy.fft import fftn, ifftn, fftshift, ifftshift
-def motion_ghosting(imageVolume, alpha, numReps, p):
-    imageHeight, imageWidth, imageDepth = imageVolume.shape  # volume size
 
-    imageVolume = fftn(imageVolume, (imageHeight, imageWidth, imageDepth))  # fast Fourier transform (k-space)
+def motion_ghosting(imageVolume, alpha, numReps, p):
+    imageHeight, imageWidth, imageDepth = imageVolume.shape
+
+    imageVolume = fftn(imageVolume, (imageHeight, imageWidth, imageDepth))
 
     if p == 1:  # along x-axis
-        imageVolume[::numReps, :, :] = alpha * imageVolume[::numReps, :, :]  # k-space lines are modulated differently
+        imageVolume[::numReps, :, :] = alpha * imageVolume[::numReps, :, :]
     elif p == 2:  # along y-axis
-        imageVolume[:, ::numReps, :] = alpha * imageVolume[:, ::numReps, :]  # k-space lines are modulated differently
+        imageVolume[:, ::numReps, :] = alpha * imageVolume[:, ::numReps, :]
     elif p == 3:  # along z-axis
-        imageVolume[:, :, ::numReps] = alpha * imageVolume[:, :, ::numReps]  # k-space lines are modulated differently
+        imageVolume[:, :, ::numReps] = alpha * imageVolume[:, :, ::numReps]
 
-    imageVolume = np.abs(ifftn(imageVolume, (imageHeight, imageWidth, imageDepth)))  # inverse ifft of modified data
+    imageVolume = np.abs(ifftn(imageVolume, (imageHeight, imageWidth, imageDepth)))
 
     return imageVolume.astype(imageVolume.dtype)
 
+img_row = 256
+img_col = 256
 
-img_row=256
-img_col=256
 def image_processing(file_path):
     data_path = sorted(os.listdir(file_path), key=lambda x: int(os.path.splitext(x)[0]))
     flair_dataset = []
@@ -145,36 +124,36 @@ def image_processing(file_path):
         img_path = os.path.join(file_path, i, 'pre')
         mask_path = os.path.join(file_path, i)
 
-        for image,mask in zip(glob.glob(img_path + '/FLAIR*'),glob.glob(mask_path + '/wmh*')):
+        for image, mask in zip(glob.glob(img_path + '/FLAIR*'), glob.glob(mask_path + '/wmh*')):
             flair_img = nib.load(image)
             flair_data = flair_img.get_fdata()
             mask_img = nib.load(mask)
             mask_data = mask_img.get_fdata()
 
-            flair_resized,mask_resized =resize_image_and_mask(flair_data,mask_data,(256,256,128))
+            flair_resized, mask_resized = resize_image_and_mask(flair_data, mask_data, (256, 256, 128))
             flair_dataset.append(flair_resized)
             mask_dataset.append(mask_resized)
 
-            #
+            # Rotation
             M1 = cv2.getRotationMatrix2D((img_row / 2, img_col / 2), 90, 1)
             flair_rotate = cv2.warpAffine(flair_resized, M1, (img_row, img_col))
             flair_dataset.append(flair_rotate)
             mask_rotate = cv2.warpAffine(mask_resized, M1, (img_row, img_col), flags=cv2.INTER_NEAREST)
             mask_dataset.append(mask_rotate)
 
-
+            # Elastic deformation
             sigma = (20 - 10) * np.random.rand() + 10
             alpha = (200 - 100) * np.random.rand() + 100
             X1, Y1 = elasticDeform(flair_resized, mask_resized, sigma=sigma, alpha=alpha)
-
             flair_dataset.append(X1)
             mask_dataset.append(Y1)
 
-            # Intensity Inhomogeneity
+            # Intensity inhomogeneity
             X2 = biasField(flair_resized)
             flair_dataset.append(X2)
             mask_dataset.append(mask_resized)
 
+            # Motion ghosting
             alpha = (0.7 - 0.5) * np.random.rand() + 0.5
             numReps = np.random.randint(2, 4)
             dim = 2
@@ -182,18 +161,14 @@ def image_processing(file_path):
             flair_dataset.append(X3)
             mask_dataset.append(mask_resized)
 
-            # sigma = 0.1 * np.random.rand()
+            # Gaussian noise
             X4 = flair_resized + np.random.normal(0, 0.1, size=flair_resized.shape)
             flair_dataset.append(X4)
             mask_dataset.append(mask_resized)
 
-
-
-
     flair_array = np.array(flair_dataset)
     mask_array = np.array(mask_dataset)
     return flair_array, mask_array
-
 
 utrecht_flair, utrecht_mask = image_processing(path1)
 amsterdam_flair, amsterdam_mask = image_processing(path2)
@@ -201,10 +176,9 @@ singapore_flair, singapore_mask = image_processing(path3)
 
 np.save('train/ori/utrecht_flair(256)aug.npy', utrecht_flair)
 np.save('train/ori/utrecht_mask(256)aug.npy', utrecht_mask)
-
 np.save('train/ori/amsterdam_flair(256)aug.npy', amsterdam_flair)
 np.save('train/ori/amsterdam_mask(256)aug.npy', amsterdam_mask)
-
 np.save('train/ori/singapore_flair(256)aug.npy', singapore_flair)
 np.save('train/ori/singapore_mask(256)aug.npy', singapore_mask)
+
 print("finish")
